4cf7e331ab2769923e9124b80fb4b998f3c9a354
Atomic649 <atomic.intergroup@gmail.com>
2025-03-24 22:26:07 +0700

    test

--- backend/index.ts
+++ backend/index.ts
@@ -23,6 +23,9 @@ app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use("/uploads/images", express.static("uploads/images"));
 app.use("/uploads/pdf", express.static("uploads/pdf"));
+app.get(`/test`, (req, res) => {
+  res.send("Hello - API");
+});
 
 // --------------IMPORT ROUTES-----------------
 
@@ -54,7 +57,7 @@ import productRoutes from "./src/routes/productRoute";
 import dashboardRoutes from "./src/routes/dashboardRoute";
 
 // Import Credit Routes
-import  creditRoutes from "./src/routes/creditRoute";
+import creditRoutes from "./src/routes/creditRoute";
 
 // Import user Routes
 import userRoutes from "./src/routes/userRoute";
@@ -66,13 +69,13 @@ import postRoutes from "./src/routes/postRoute";
 import commentRoutes from "./src/routes/commentRoute";
 
 // Import report Routes
-import  reportRoutes  from "./src/routes/reportRoute";
+import reportRoutes from "./src/routes/reportRoute";
 
 // Import pdf Rou
 import pdfRoutes from "./src/routes/pdfRoute";
 
 // Import Store Routes
-import storeRoutes  from "./src/routes/storeRoute";
+import storeRoutes from "./src/routes/storeRoute";
 
 // --------------USE ROUTES-----------------
 
